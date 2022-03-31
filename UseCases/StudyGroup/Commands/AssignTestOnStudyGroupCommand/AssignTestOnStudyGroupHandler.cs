0876ad2b969aa206505c76d3d8a706f2336988fc
timofey.klimov@inbox.ru <timofey.klimov@inbox.ru>
2022-03-31 23:53:42 +0300

    создал реквест, хэндлер

---
+++ UseCases/StudyGroup/Commands/AssignTestOnStudyGroupCommand/AssignTestOnStudyGroupHandler.cs
@@ -0,0 +1,25 @@
+﻿using DataAccess.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UseCases.StudyGroup.Commands.AssignTestOnStudyGroupCommand
+{
+    public class AssignTestOnStudyGroupHandler : IRequestHandler<AssignTestOnStudyGroupRequest>
+    {
+        private readonly IDbContext _dbContext;
+        public AssignTestOnStudyGroupHandler(IDbContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public async Task<Unit> Handle(AssignTestOnStudyGroupRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UseCases/StudyGroup/Commands/AssignTestOnStudyGroupCommand/AssignTestOnStudyGroupRequest.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseCases.StudyGroup.Commands.AssignTestOnStudyGroupCommand
+{
+    public record AssignTestOnStudyGroupRequest(int GroupId, int TestId, DateTime Deadline) : IRequest;
+  
+}
