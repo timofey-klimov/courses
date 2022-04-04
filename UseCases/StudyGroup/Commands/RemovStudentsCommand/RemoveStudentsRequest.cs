9dfa8a633ec08349e3c8be5ef1822d5dbfdd8f3e
Dmitriy <selezmsu@yandex.ru>
2022-04-04 21:55:21 +0300

    Метод для удаления учеников из группы

---
+++ UseCases/StudyGroup/Commands/RemovStudentsCommand/RemoveStudentsRequest.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCases.StudyGroup.Dto;
+
+namespace UseCases.StudyGroup.Commands.RemovStudentsCommand
+{
+    public record RemoveStudentsRequest(int studyGroupId, IEnumerable<int> studentsId) : IRequest<RemoveStudentsFromStudyGroupDto>;
+}
